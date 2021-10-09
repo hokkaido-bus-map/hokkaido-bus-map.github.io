72bca130f8baf6f46981761f23fa3541c2fb329b
Kanahiro <mediterranean1769@gmail.com>
2021-10-09 17:44:56 +0900

    add sightseeing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-    <header>Hokkaido Sightseeing Bus map</header>
+    <header>
+        <h2>Hokkaido Bus map</h2>
+        <p>
+            さまざまなオープンデータを利用して、バスの運行頻度・コンビニ・観光地を地図にしました。
+        </p>
+    </header>
 </template>
 <script>
 export default {
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -8,14 +8,17 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 
 import { getFreqSourcesLayers } from '../loader';
 
+import sightseeing from '../assets/sightseeing.json';
 import kitamiFreqGeojson from '../assets/busdata/kitami/frequency.json';
 import kitamiFreqStopsGeojson from '../assets/busdata/kitami/frequency_stops.json';
+import kitamimemanbetsuairportFreqGeojson from '../assets/busdata/kitamimemanbetsuairport/frequency.json';
+import kitamimemanbetsuairportFreqStopsGeojson from '../assets/busdata/kitamimemanbetsuairport/frequency_stops.json';
 import abashiriFreqGeojson from '../assets/busdata/abashiri/frequency.json';
 import abashiriFreqStopsGeojson from '../assets/busdata/abashiri/frequency_stops.json';
+import abashirishisetsumeguriFreqGeojson from '../assets/busdata/abashirishisetsumeguri/frequency.json';
+import abashirishisetsumeguriFreqStopsGeojson from '../assets/busdata/abashirishisetsumeguri/frequency_stops.json';
 import akanFreqGeojson from '../assets/busdata/akan/frequency.json';
 import akanFreqStopsGeojson from '../assets/busdata/akan/frequency_stops.json';
-import memanbetsuFreqGeojson from '../assets/busdata/memanbetsu/frequency.json';
-import memanbetsuFreqStopsGeojson from '../assets/busdata/memanbetsu/frequency_stops.json';
 import takushokuFreqGeojson from '../assets/busdata/takushoku/frequency.json';
 import takushokuFreqStopsGeojson from '../assets/busdata/takushoku/frequency_stops.json';
 import tokachiFreqGeojson from '../assets/busdata/tokachi/frequency.json';
@@ -31,15 +34,20 @@ const abashiriFreq = getFreqSourcesLayers(
     abashiriFreqStopsGeojson,
     'abashiri',
 );
+const abashirishisetsumeguriFreq = getFreqSourcesLayers(
+    abashirishisetsumeguriFreqGeojson,
+    abashirishisetsumeguriFreqStopsGeojson,
+    'abashirishisetsumeguri',
+);
 const akanFreq = getFreqSourcesLayers(
     akanFreqGeojson,
     akanFreqStopsGeojson,
     'akan',
 );
-const memanbetsuFreq = getFreqSourcesLayers(
-    memanbetsuFreqGeojson,
-    memanbetsuFreqStopsGeojson,
-    'memanbetsu',
+const kitamimemanbetsuairportFreq = getFreqSourcesLayers(
+    kitamimemanbetsuairportFreqGeojson,
+    kitamimemanbetsuairportFreqStopsGeojson,
+    'kitamimemanbetsuairport',
 );
 const takushokuFreq = getFreqSourcesLayers(
     takushokuFreqGeojson,
@@ -56,6 +64,7 @@ const initMap = () => {
     const map = new Map({
         container: 'map',
         center: [142.9819, 40.3981],
+        maxBounds: [139, 40, 150, 46],
         zoom: 6,
         style: {
             version: 8,
@@ -70,10 +79,15 @@ const initMap = () => {
                 },
                 ...kitamiFreq.sources,
                 ...abashiriFreq.sources,
+                ...abashirishisetsumeguriFreq.sources,
                 ...akanFreq.sources,
-                ...memanbetsuFreq.sources,
+                ...kitamimemanbetsuairportFreq.sources,
                 ...takushokuFreq.sources,
                 ...tokachiFreq.sources,
+                sightseeing: {
+                    type: 'geojson',
+                    data: sightseeing,
+                },
             },
             layers: [
                 {
@@ -85,10 +99,57 @@ const initMap = () => {
                 },
                 ...kitamiFreq.layers,
                 ...abashiriFreq.layers,
+                ...abashirishisetsumeguriFreq.layers,
                 ...akanFreq.layers,
-                ...memanbetsuFreq.layers,
+                ...kitamimemanbetsuairportFreq.layers,
                 ...takushokuFreq.layers,
                 ...tokachiFreq.layers,
+                {
+                    id: 'sightseeing',
+                    type: 'circle',
+                    source: 'sightseeing',
+                    paint: {
+                        'circle-color': '#ff9900',
+                        'circle-stroke-color': '#ffffff',
+                        'circle-stroke-width': [
+                            'interpolate',
+                            ['linear'],
+                            ['zoom'],
+                            6,
+                            1,
+                            15,
+                            2,
+                        ],
+                        'circle-radius': [
+                            'interpolate',
+                            ['linear'],
+                            ['zoom'],
+                            6,
+                            4,
+                            10,
+                            5,
+                            15,
+                            6,
+                        ],
+                    },
+                },
+                {
+                    id: 'sightseeingText',
+                    type: 'symbol',
+                    source: 'sightseeing',
+                    minzoom: 7,
+                    layout: {
+                        'text-field': ['get', '施設名/名称'],
+                        'text-font': ['Noto Sans CJK JP Regular'],
+                        'text-size': 10,
+                        'text-offset': [0, 0.5],
+                        'text-anchor': 'top',
+                    },
+                    paint: {
+                        'text-halo-width': 1.5,
+                        'text-halo-color': '#ffffff',
+                    },
+                },
             ],
         },
     });
